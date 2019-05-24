f2cf0017327164ae596692f598488bf5680ecd34
yubinhong <704523059@qq.com>
2019-05-24 11:16:08 +0800

    #优化

--- web/views.py
+++ web/views.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import HttpResponse
 import json
 # Create your views here.
 
 
 def index(request):
     data = {}
-    remote_addr = request.META['REMOTE_ADDR']
-    data['remote_addr'] = remote_addr
-    if request.META.has_key('HTTP_X_FORWARDED_FOR'):
+    if 'HTTP_X_FORWARDED_FOR' in request.META:
         http_x_forward_for = request.META['HTTP_X_FORWARDED_FOR']
-        data['http_x_forward_for'] = http_x_forward_for
+        data['ip'] = http_x_forward_for
+    else:
+        data['ip'] = request.META['REMOTE_ADDR']
 
     data = json.dumps(data)
     return HttpResponse(data)
