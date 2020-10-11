f0c4537fedbf3a1363e28214df4528140c7a6c85
hex <hex@tzc888.com>
2020-10-11 11:20:47 +0800

    #优化

--- web/views.py
+++ web/views.py
@@ -14,7 +14,11 @@ def index(request):
         data = {}
         ip_to_region = ip2Region.Ip2Region(settings.ip_data_path)
         if "ip" not in request.GET:
-            data['ip'] = request.META['REMOTE_ADDR']
+            if 'HTTP_X_Real_IP' in request.META:
+                real_ip = request.META['HTTP_X_Real_IP']
+                data['ip'] = real_ip
+            else:
+                data['ip'] = request.META['REMOTE_ADDR']
         else:
             data['ip'] = request.GET.get('ip')
         if tools.ipv6_check(data['ip']):
